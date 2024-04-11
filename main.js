16a57ee3bc3f5b13b86c4e1cf3e05373012f9088
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2024-04-11 08:35:08 -0300

    dATA

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-04T00:00:00");
+const tempoObjetivo2 = new Date("2025-03-02T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo1 = new Date("2024-11-01T00:00:00");
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
