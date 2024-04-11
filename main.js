aa9065c1b732619f61c905121e043cba0c14decc
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2024-04-11 08:40:02 -0300

    final

--- main.js
+++ main.js
@@ -36,6 +36,9 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}
+
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    }else{
+        return "Prazo encerrado";
+    }
